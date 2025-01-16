be74dc247df2d898c6457d82ad59de2a665f92ab
Anshul <anshulrokadeofficial@gmail.com>
2025-01-16 18:28:46 +0530

    Added more htmls

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-const hello = 'Hello World';
-console.log(hello);
-const Cars = ['bmw','volvo','audi'];
-console.log(Cars);
+// const hello = 'Hello World';
+// console.log(hello);
+// const Cars = ['bmw','volvo','audi'];
+// console.log(Cars);
 
 // This is Blocking, sychronous way to read and write file
 const fs = require('fs'); //fs = file system
+const http = require('http'); //include server or package.
+const url = require('url'); // for routing
 
+/////////////////////////////////////////////////////////////////////////////
+//Files
+/*
 const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')  //sync=synchronise //utf-8 is used we are using english
 console.log(textIn); //read file
 
@@ -34,4 +39,34 @@ fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
     });
 });
 console.log('Will read file!');
+*/
+/*
+Syntax of callback function
+const.variablName((data, data) => {
+commands like console.log
+});
+*/
+////////////////////////////////////////////////
+
+//Server
+//This is a callback function
+const server = http.createServer((req, res) => { //request and response function in bracket
+
+    const pathName = req.url; //Routing
+    if(pathName === '/' || pathName === '/overview') {
+        res.end('This is the Overview!');
+    } else if (pathName === '/product') {
+        res.end('This is Product!');
+    } else {
+        res.writeHead(404, { //Header
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        });
+        res.end('Page not Found!!!');
+    }
+});
+
+server.listen(8000, '127.0.0.1', () => { //8000 = port, 127.0.0.1 = host
+    console.log('Listening requests on port 8000');
+});
 
